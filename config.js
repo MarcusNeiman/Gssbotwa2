3d6f163e6204f985d785b2e298f6f2bb480d8ac7
MarcusNeiman <168633731+MarcusNeiman@users.noreply.github.com>
2024-08-01 16:33:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
+global.ownername = "Neiman";
+global.owner = ['254725693306'];
 global.premium = ['917050906659'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "Marcus";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -33,8 +33,8 @@ global.mess = {
     bangc: '`This Group is *Banned* from using Commands!`',
     success: '`✓ Success`',
     admin: '`This feature is only for group admins`',
-    botAdmin: '`I am not an admin!`',
-    owner: '`You are not my owner`',
+    botAdmin: '`marcus tell them to make me the admin first`',
+    owner: '`I only take commands from Marcus`',
     group: '`You can use this command only in groups ❌`',
     private: '`Feature is used only for private chats!`',
     bot: '`Bot number user special features`',
